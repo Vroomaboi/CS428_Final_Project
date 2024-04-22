a5235a5e8ce2b0ccf031b2f880ca57fc68b5962a
Vroomaboi <109242149+Vroomaboi@users.noreply.github.com>
2024-04-22 14:26:27 -0500

    added starter code

--- proxy.c
+++ proxy.c
@@ -1,4 +1,16 @@
-#include <stdio.h>
+/*
+ * proxy.c - CS:APP Web proxy
+ *
+ * TEAM MEMBERS:  put your name(s) and e-mail addresses here
+ *     Howard the Duck, howie@duck.sewanee.edu
+ *     James Q. Pleebus, pleebles@q.sewanee.edu
+ * 
+ * IMPORTANT: Give a high level description of your code here. You
+ * must also provide a header comment at the beginning of each
+ * function that describes what that function does.
+ */ 
+
+#include "csapp.h"
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -7,8 +19,105 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+/*
+ * Function prototypes
+ */
+int parse_uri(char *uri, char *target_addr, char *path, int  *port);
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, int size);
+
+/* 
+ * main - Main routine for the proxy program 
+ */
+int main(int argc, char **argv)
 {
+
+    /* Check arguments */
+    if (argc != 2) {
+	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
+	exit(0);
+    }
+
     printf("%s", user_agent_hdr);
     return 0;
 }
+
+
+/*
+ * parse_uri - URI parser
+ * 
+ * Given a URI from an HTTP proxy GET request (i.e., a URL), extract
+ * the host name, path name, and port.  The memory for hostname and
+ * pathname must already be allocated and should be at least MAXLINE
+ * bytes. Return -1 if there are any problems.
+ */
+int parse_uri(char *uri, char *hostname, char *pathname, int *port)
+{
+    char *hostbegin;
+    char *hostend;
+    char *pathbegin;
+    int len;
+
+    if (strncasecmp(uri, "http://", 7) != 0) {
+	hostname[0] = '\0';
+	return -1;
+    }
+       
+    /* Extract the host name */
+    hostbegin = uri + 7;
+    hostend = strpbrk(hostbegin, " :/\r\n\0");
+    len = hostend - hostbegin;
+    strncpy(hostname, hostbegin, len);
+    hostname[len] = '\0';
+    
+    /* Extract the port number */
+    *port = 80; /* default */
+    if (*hostend == ':')   
+	*port = atoi(hostend + 1);
+    
+    /* Extract the path */
+    pathbegin = strchr(hostbegin, '/');
+    if (pathbegin == NULL) {
+	pathname[0] = '\0';
+    }
+    else {
+	pathbegin++;	
+	strcpy(pathname, pathbegin);
+    }
+
+    return 0;
+}
+
+/*
+ * format_log_entry - Create a formatted log entry in logstring. 
+ * 
+ * The inputs are the socket address of the requesting client
+ * (sockaddr), the URI from the request (uri), and the size in bytes
+ * of the response from the server (size).
+ */
+void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, 
+		      char *uri, int size)
+{
+    time_t now;
+    char time_str[MAXLINE];
+    unsigned long host;
+    unsigned char a, b, c, d;
+
+    /* Get a formatted time string */
+    now = time(NULL);
+    strftime(time_str, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    /* 
+     * Next, convert the IP address in network byte order to dotted decimal
+     * form. Note that we could have used inet_ntoa, but chose not to
+     * because inet_ntoa is a Class 3 thread unsafe function that
+     * returns a pointer to a static variable (Ch 13, CS:APP).
+     */
+
+    // for the student to do...
+    
+    /* Finally, store (and return) the formatted log entry string in logstring */
+
+    // for the student to do...
+
+}
+
